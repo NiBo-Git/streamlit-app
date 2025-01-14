8927140d2e45797c76714589b8ab49246e53b99a
NiBo-Git <n.bhardt@gmail.com>
2025-01-14 15:22:58 +0000

    upload button

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+import pandas as pd
+
 
 st.title("new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+uploaded_file = st.file_uploader("Choose a file")
+
+#read excel
+
+if uploaded_file:
+    df1=pd.read_excel(uploaded_file)
+    print(df1)
+else:
+    print("Empty")
